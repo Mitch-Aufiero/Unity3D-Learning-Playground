c2b507759d338b731eb9a7cd7d4db193c685fc32
Mitch Aufiero <mitch.aufiero@gmail.com>
2021-03-06 12:11:30 -0600

    spider animation and colliders

---
+++ Assets/Scripts/SpiderMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour
+{
+    // if leg is ready to move(idk how to tell this)
+    // start coroutine of removing leg rig weight once done set target to new position(cast ray) set weight back to 1
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+        
+
+}
